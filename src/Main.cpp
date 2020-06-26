1c00746e2e965d3a16f9c0d0d53bc4018f29c041
drakeee <drakedrift@gmail.com>
2020-06-26 22:11:13 +0200

    Initial commit.

---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+﻿// altv-lua-module.cpp : Defines the entry point for the application.
+//
+
+#include "Main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
