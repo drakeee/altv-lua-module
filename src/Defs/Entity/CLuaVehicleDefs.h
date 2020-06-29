5f9b6f8ebc033711a0eb5adaa5e4197fd15714f6
NeQYT <46975849+NeQYT@users.noreply.github.com>
2020-06-29 21:25:44 +0200

    Update CLuaVehicleDefs.h

--- src/Defs/Entity/CLuaVehicleDefs.h
+++ src/Defs/Entity/CLuaVehicleDefs.h
@@ -11,4 +11,12 @@ public:
 	static int CreateVehicle(lua_State* L);
 	static int SetVehiclePosition(lua_State* L);
 	static int GetVehiclePosition(lua_State* L);
+	static int SetVehicleBodyHealth(lua_State* L);
+	static int SetVehicleEngineHealth(lua_State* L);
+	static int GetVehicleBodyHealth(lua_State* L);
+	static int GetVehicleEngineHealth(lua_State* L);
+	static int SetVehiclePrimaryColor(lua_State* L);
+	static int SetVehicleSecondaryColor(lua_State* L);
+	static int GetVehiclePrimaryColor(lua_State* L);
+	static int GetVehicleSecondaryColor(lua_State* L);
 };
