cf83e18796f1f593f694ee9d9f573b3588e9877f
Trevor Wessel <stuykgaming@gmail.com>
2020-07-23 15:40:44 -0600

    Attempt Zipping

--- build.bat
+++ build.bat
@@ -13,9 +13,8 @@ cd ..
 xcopy vendors\luajit\bin\win64\lua51.dll build\win64\Release\modules\lua-module\ /Y
 xcopy build\win64\Release\lua-module.dll build\win64\Release\modules\lua-module\ /Y
 
-mkdir module
 mkdir module\lua-module
-xcopy build\win64\Release\modules\lua-module\ module\lua-module
+xcopy /s build\win64\Release\modules\lua-module\ module\lua-module /Y
 
-7z a -tzip lua-module-windows.zip -r module\*
+7z a -tzip lua-module-windows.zip -r module
 
