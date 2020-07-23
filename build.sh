861b5de2f57f499ff4b4ea3619448189d71cb5b3
drakeee <drakedrift@gmail.com>
2020-07-23 19:53:37 +0200

    build fix.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git submodule update --init
+git submodule update --init --recursive --remote
 cd vendors/luajit/ && make
 mkdir -p {include,bin/linux64,lib/linux64}
 cp ./src/{lauxlib.h,lua.h,lua.hpp,luaconf.h,luajit.h,lualib.h} ./include/
@@ -9,4 +9,10 @@ cp ./src/{luajit,libluajit.so} ./bin/linux64/
 mv ./bin/linux64/libluajit.so ./bin/linux64/lua51.so
 
 cd ../..
-cmake ./ && make
+cmake -DCMAKE_BUILD_TYPE=Release ./
+cmake --build . --config Release
+
+mkdir -p ./build/linux64/Release/modules/lua-module
+cp ./vendors/luajit/bin/linux64/lua51.so ./build/linux64/Release/modules/lua-module/
+cp ./build/linux64/Release/liblua-module.so ./build/linux64/Release/modules/lua-module/
+
