e19658976c994a2c3741debe853c75ee009f5398
Trevor Wessel <stuykgaming@gmail.com>
2020-07-23 17:03:05 -0600

    Remove Extra Steps

--- build.bat
+++ build.bat
@@ -13,9 +13,5 @@ cd ..
 xcopy vendors\luajit\bin\win64\lua51.dll build\win64\Release\modules\lua-module\ /Y
 xcopy build\win64\Release\lua-module.dll build\win64\Release\modules\lua-module\ /Y
 
-mkdir lua-module
-xcopy /s build\win64\Release\modules\lua-module\lua51.dll lua-module /Y
-xcopy /s build\win64\Release\modules\lua-module\lua-module.dll lua-module /Y
-
-7z a -tzip lua-module-windows.zip -r lua-module
+7z a -tzip lua-module-windows.zip -r build\win64\Release\modules\lua-module
 
