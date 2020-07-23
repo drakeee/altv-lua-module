861952b09e4f038cf723ceb825dc9a3b643585d5
drakeee <drakedrift@gmail.com>
2020-07-23 22:35:41 +0200

    Build fixes.

--- build.bat
+++ build.bat
@@ -1,19 +1,7 @@
 @echo off
 
 git submodule update --init --recursive --remote
-REM cd vendors/luajit/src/
-REM call msvcbuild.bat
 
-REM cd ..
-REM if not exist include mkdir include
-REM if not exist bin\\win64 mkdir bin\\win64
-REM if not exist lib\\win64 mkdir lib\\win64
-
-REM for %%I in (src\lauxlib.h src\lua.h src\lua.hpp src\luaconf.h src\luajit.h src\lualib.h) do xcopy %%I include\\ /Y
-REM for %%I in (src\lua51.lib src\luajit.lib) do xcopy %%I lib\\win64\\ /Y
-REM for %%I in (src\lua51.dll src\luajit.exe) do xcopy %%I bin\\win64\\ /Y
-
-REM cd ..\\..
 if not exist build mkdir build
 cd build
 
