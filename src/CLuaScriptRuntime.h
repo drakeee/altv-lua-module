5b5b2a0b41543f8ae93aa90fffb1df21922dac0d
drakeee <drakedrift@gmail.com>
2020-09-19 20:52:06 +0200

    Changing module version.

--- src/CLuaScriptRuntime.h
+++ src/CLuaScriptRuntime.h
@@ -50,7 +50,7 @@ public:
 	~CLuaScriptRuntime() {};
 
 private:
-	const semver::version						version{ 0, 3, 19, semver::prerelease::dev };
+	const semver::version						version{ 0, 3, 20, semver::prerelease::dev };
 	std::map<lua_State*, CLuaResourceImpl*>		resources;
 	EventsCallbacks								eventsCallbacks;
 	EventsGetter								eventsGetter;
