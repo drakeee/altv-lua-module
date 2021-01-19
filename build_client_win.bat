d81c4f911b9a36cf911966b2c8dd0947754207c0
drakeee <drakedrift@gmail.com>
2021-01-19 19:37:28 +0100

    Fix CI #3

--- build_client_win.bat
+++ build_client_win.bat
@@ -16,10 +16,10 @@ xcopy libs\lua51\win64\lua51.dll artifacts\lua-client-module\ /Y
 REM xcopy libs\mariadb\win64\libmariadb.dll build\win64\Release\modules\lua-module\ /Y
 REM xcopy libs\sqlite3\win64\sqlite3.dll build\win64\Release\modules\lua-module\ /Y
 xcopy build\win64\Release\lua-client-module.dll artifacts\lua-client-module\ /Y
-xcopy build\win64\Release\lua-client-module-static.dll artifacts\lua-client-module\ /Y
+xcopy build\win64\Release\lua-client-module-static.lib artifacts\ /Y
 
 cd artifacts
-if not exist "lua-client-module\modules" mkdir "lua-client-module\modules"
+REM if not exist "lua-client-module\modules" mkdir "lua-client-module\modules"
 7z a -tzip "lua-client-module-windows.zip" -r lua-client-module
 
 cd ..
